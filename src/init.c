ffa5e7d4f8e02b1f95b018f457451e0a86e70fb1
Pablo Markaide <pablo.markaide@gmail.com>
2024-11-04 14:45:10 +0200

    fix: initialize macro to NULLs

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:45:44 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/10/31 08:32:16 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:44:07 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,19 @@ t_macro	*init_macro(t_macro *macro)
 	macro = malloc(sizeof(t_macro));
 	if (!macro)
 		return (NULL);
+	bzero(macro, sizeof(t_macro));
 	macro->map = malloc(sizeof(t_map));
 	if (!macro->map)
 	{
 		free(macro);
 		return (NULL);
 	}
+	bzero(macro->map, sizeof(t_map));
+	macro->map->no = NULL;
+	macro->map->so = NULL;
+	macro->map->we = NULL;
+	macro->map->ea = NULL;
+	macro->map->map = NULL;
 	return (macro);
 }
 
