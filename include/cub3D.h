3c4a279327ae3bb89107bdde3670112a70a68ab1
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-30 20:23:44 +0200

    chore: sort header file functions

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:46:43 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/12/30 20:17:52 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/12/30 20:23:10 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,60 +101,52 @@ typedef struct s_macro
 	int				height;
 }					t_macro;
 
-int					is_valid_position(t_macro *m, char **visited, int x, int y);
-void				flood_fill(t_macro *m, char **visited, int x, int y);
-char				**create_visited_array(int height, int width);
-void				free_visited_array(char **visited, int height);
-int					check_islands(t_macro *m, char **visited);
-void				free_macro(t_macro *m);
-void				free_all(t_macro *m);
-void				release_all(mlx_key_data_t keydata, t_macro *m);
-void				ft_hook(mlx_key_data_t keydata, void *param);
-int					get_rgba(int r, int g, int b, int a);
-t_macro				*init_macro(t_macro *m);
-int					is_valid_fc_format(char *line);
-int					parse_color_values(char *skipped, int *color, int *err,
-						t_macro *m);
-int					parse_line(char *line, t_macro *m, int section,
-						t_list **head);
-int					check_file_contents(char *file, t_macro *m);
-void				clean_trailing_char(char *str, const char *set);
-int					save_texture_path(char **texture, char *path);
-int					map_line_is_correct(char *line);
-int					read_input(char *file, t_macro *m);
-int					init_game(t_macro *m);
-void				move_wsda(t_macro *m, double x, double y);
-void				get_wsda(t_macro *m, double x, double y);
-void				move(t_macro *m);
-void				load_game(void *param);
-void				load_player(t_macro *m);
-void				player_in_map(t_macro *m);
-void				mirror_vert(mlx_texture_t *texture, int left, int right);
-void				load_map(t_macro *m);
-int					make_color(int c);
-int					radian_side(double angle, int side);
-int					cross_lines(double angle, float *crossed, float *step,
-						int normalize);
-int					wall_hit(float x, float y, t_macro *m);
-void				draw_wall(t_macro *m, int top_wall, int bottom,
-						double wall_h);
-void				do_wall(t_macro *m, int vertical);
-void				raycast(t_macro *m);
-float				x_dda(t_macro *m, double angle, double dda);
-float				y_dda(t_macro *m, double angle, double dda);
-float				normalize(float angle);
-mlx_texture_t		*get_texture(t_macro *m, int flag);
-double				x_off(mlx_texture_t *texture, t_macro *m);
 void				calculate_map_dimensions(t_macro *m);
-void				fill_with_spaces(char **map, int width, t_macro *m);
-void				substitute_spaces_with_zeros(t_macro *m);
+int					check_file_contents(char *file, t_macro *m);
+int					check_islands(t_macro *m, char **visited);
 int					check_unique_starting_position(t_macro *m);
-void				map_chars_are_valid(t_macro *m);
-void				validate_map(t_macro *m);
-int					validation(t_macro *m);
-char				*get_skipped(char *line);
-void				free_map(t_macro *m);
+void				clean_trailing_char(char *str, const char *set);
 void				create_buffer_zone(t_macro *m);
+char				**create_visited_array(int height, int width);
+void				draw_wall(t_macro *m, int top_wall, int bottom, double wall_h);
+void				do_wall(t_macro *m, int vertical);
+void				fill_with_spaces(char **map, int width, t_macro *m);
 void				finish_reading_the_file(int fd, char *line);
+void				flood_fill(t_macro *m, char **visited, int x, int y);
+void				free_all(t_macro *m);
+void				free_macro(t_macro *m);
+void				free_map(t_macro *m);
+void				free_visited_array(char **visited, int height);
+void				ft_hook(mlx_key_data_t keydata, void *param);
+void				get_wsda(t_macro *m, double x, double y);
+char				*get_skipped(char *line);
+int					get_rgba(int r, int g, int b, int a);
+mlx_texture_t		*get_texture(t_macro *m, int flag);
+int					init_game(t_macro *m);
+t_macro				*init_macro(t_macro *m);
+void				load_game(void *param);
+void				load_map(t_macro *m);
+void				load_player(t_macro *m);
+void				map_chars_are_valid(t_macro *m);
+int					make_color(int c);
+void				mirror_vert(mlx_texture_t *texture, int left, int right);
+void				move(t_macro *m);
+void				move_wsda(t_macro *m, double x, double y);
+float				normalize(float angle);
+int					parse_color_values(char *skipped, int *color, int *err, t_macro *m);
+int					parse_line(char *line, t_macro *m, int section,t_list **head);
+void				player_in_map(t_macro *m);
+int					radian_side(double angle, int side);
+void				raycast(t_macro *m);
+int					read_input(char *file, t_macro *m);
+void				release_all(mlx_key_data_t keydata, t_macro *m);
+int					save_texture_path(char **texture, char *path);
+void				substitute_spaces_with_zeros(t_macro *m);
+int					validation(t_macro *m);
+void				validate_map(t_macro *m);
+int					wall_hit(float x, float y, t_macro *m);
+float				x_dda(t_macro *m, double angle, double dda);
+double				x_off(mlx_texture_t *texture, t_macro *m);
+float				y_dda(t_macro *m, double angle, double dda);
 
 #endif
